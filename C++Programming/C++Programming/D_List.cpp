79c30aa2d588c55ee360617ecd953d850afd78b1
tlfzgjadyd <tlfzgjadyd@naver.com>
2021-03-31 17:14:19 +0900

    교수님 예제 추가

--- C++Programming/C++Programming/D_List.cpp
+++ C++Programming/C++Programming/D_List.cpp
@@ -82,7 +82,7 @@ int List::removeItem(int itm) {//리스트에 있는 특정 값을 찾아서 삭제하는 int rem
 void List::concat(List& list) {//리스트의 접합 연산을 수행하는 concat(List& list) 함수 구현 ( 2개의 리스트를 연결  - 자기자신과 파라미터로 전달된 리스트 )
         //반환형이 명시되어 있지 않지만 void로 지정하여 함수를 호출한 주체에 전달인자로 받은 리스트를 이어붙이는 방향으로 구현했습니다.
     int newSize = this->itemCount + list.itemCount;
-    List newList(newSize);
+    List newList(newSize);//새로운 List형을 만들어서 편집한 후 this의 모든걸 이 새로운 List의 것으로 교체
     for (int i = 0; i < this->itemCount; i++)
     {
         newList.addItem(this->items[i]);
@@ -92,7 +92,7 @@ void List::concat(List& list) {//리스트의 접합 연산을 수행하는 concat(List& list
     }
     this->size = newList.size;
     delete[] items;
-    this->items = new int[size];
+    this->items = new int[size];//다시 공간할당 해준다
     this->itemCount = 0;
     for (int i = 0; i < newList.itemCount; i++) {
         this->addItem(newList.items[i]);
@@ -157,6 +157,32 @@ void OrderedList::addItem(int itm) {//리스트에 값을 추가하는 void addItem(int it
             cout << "리스트가 꽉차서 추가할 수 없습니다" << endl;
         }
 }
+//교수님 예시 추가한 버전 공간 부족시 공간 늘리기
+/*void OrderedList::addItem(int itm)//리스트에 값을 추가하는 함수
+{
+    if (itemCount >= size)
+    {
+        int newSize = size * 2;
+        int* newItem = new int[newSize];
+        for (int i = 0; i < itemCount; i++)
+            newItem[i] = items[i];
+        delete[]items;
+        items = new int[newSize];
+        for (int j = 0; j < itemCount; j++)
+            items[j] = newItem[j];
+        size = newSize;
+    }
+
+    int pos;
+    for (pos = 0; pos < itemCount; pos++) {
+        if (items[pos] > itm)
+            break;
+    }
+    itemCount++;
+    for (int i = itemCount - 1; i > pos; i--)
+        items[i] = items[i - 1];
+    items[pos] = itm;
+}*/
 int OrderedList::removeAt(int index) {//리스트의 특정 위치에 있는 값을 삭제하는 int removeAt(int index) 함수 작성 ( 성공시 삭제된 값이 반환, 실패시(인덱스오류) -99999 반환)
         if (index < size && index < itemCount && items[index] != NULL)
         {
@@ -189,7 +215,7 @@ int OrderedList::removeItem(int itm) {//리스트에 있는 특정 값을 찾아서 삭제하는 
 void OrderedList::concat(OrderedList& olist) {//리스트의 접합 연산을 수행하는 void concat(OrderedList& olist) 함수 구현 ( 2개의 리스트를 연결 - 연결후에도 정렬된 상태 유지 - 자기자신과 파라미터로 전달된 리스트 )
         //반환형이 void로 명시되어 있어 함수를 호출한 주체에 전달인자로 받은 리스트를 이어붙이는 방향으로 구현했습니다.
     int newSize = this->itemCount + olist.itemCount;
-    OrderedList newList(newSize);
+    OrderedList newList(newSize);//새로운 OrderedList형을 만들어서 편집한 후 this의 모든걸 이 새로운 OrderedList의 것으로 교체
     for (int i = 0; i < this->itemCount; i++)
     {
         newList.addItem(this->items[i]);
@@ -199,7 +225,7 @@ void OrderedList::concat(OrderedList& olist) {//리스트의 접합 연산을 수행하는 vo
     }
     this->size = newList.size;
     delete[] items;
-    this->items = new int[size];
+    this->items = new int[size];//다시 공간할당 해준다
     this->itemCount = 0;
     for (int i = 0; i < newList.itemCount; i++) {
         this->addItem(newList.items[i]);
